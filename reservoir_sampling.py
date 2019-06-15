6362d63b94243eed7e1e0f8925cded2935a4badb
Simion-Constantinescu Andrei <andrei.simion.c@gmail.com>
2019-06-15 14:18:06 +0200

    Finished reservoir sampling without comparing

--- reservoir_sampling.py
+++ reservoir_sampling.py
@@ -1,11 +1,26 @@
 from queue import PriorityQueue
+from collections import Counter
 from tqdm import tqdm
 
 import numpy as np
 
 from logger import Logger
+from utils import load_data, get_estimation_error
 
-from utils import load_data
+def true_count(data_df, logger):
+
+  host_df = data_df[data_df['ip_src'] == logger.config_dict['INFECTED_HOST']]
+
+  ip_occurences = Counter(host_df['ip_dest'].values).items()
+  ip_occurences = sorted(ip_occurences, key=lambda x: x[1], reverse = True)
+
+  return ip_occurences
+
+
+def estimated_count(reservoir_list):
+
+  return sorted(Counter(reservoir_list).items(), key=lambda x: x[1], reverse = True)
+
 
 def reservoir_sample(data_df, reservoir_size, logger):
 
@@ -15,7 +30,7 @@ def reservoir_sample(data_df, reservoir_size, logger):
 
     if row['ip_src'] != logger.config_dict['INFECTED_HOST']:
       continue
-      
+
     rank = np.random.uniform(low = 0.0, high = 1.0)
     if reservoir_pq.qsize() < reservoir_size:
       reservoir_pq.put((-rank, row['ip_dest']), block = False)
@@ -23,10 +38,9 @@ def reservoir_sample(data_df, reservoir_size, logger):
       crt_pair = reservoir_pq.get(block = False)
       reservoir_pq.put(crt_pair if -crt_pair[0] < rank else (-rank, row['ip_dest']))
 
-  print(reservoir_pq)
   reservoir = []
-  while reservoir_pq.empty():
-    reservoir.append(reservoir_pq.get(block = False))
+  while not reservoir_pq.empty():
+    reservoir.append(reservoir_pq.get(block = False)[1])
 
   return reservoir
 
@@ -36,4 +50,14 @@ if __name__ == "__main__":
   logger = Logger(show = True, html_output = True, config_file = "config.txt")
 
   df = load_data("DATA_FILE1", logger)
-  reservoir = reservoir_sample(df, 50, logger)
+
+  reservoir_size = 50
+  reservoir = reservoir_sample(df, reservoir_size, logger)
+
+  true_ip_freq = true_count(df, logger)
+  estimated_ip_freq = estimated_count(reservoir)
+
+  max_err = sum([elem[1] for elem in true_ip_freq[:10]])
+  print(max_err)
+  crt_err = get_estimation_error(true_ip_freq, estimated_ip_freq, 10)
+  print(crt_err)
