599d890c41fc635348ecf9480674bcecabb02c70
mihaivo <mihai1voicescu@gmail.com>
2019-06-26 12:52:33 +0200

    Bonus

--- utils.py
+++ utils.py
@@ -3,6 +3,63 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from collections import Counter
+from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+
+
+def get_train_test_data(df, logger):
+    df_selected = df[['ip_src', 'duration', 'protocol', 'flags', 'packets', 'bytes', 'label']]
+    logger.log("Select columns for classification: {}".format(df_selected.columns))
+
+    df_selected = df_selected[df_selected['label'] != "Background"]
+    logger.log("Remove background flows")
+
+    df_selected['protocol'] = pd.Categorical(df_selected['protocol']).codes
+    df_selected['flags'] = pd.Categorical(df_selected['flags']).codes
+    df_selected.replace({'label': {"LEGITIMATE": 0, "Botnet": 1}}, inplace=True)
+    logger.log("Convert string columns to numerical codes")
+
+    infected_host_train = logger.config_dict['INFECTED_HOSTS'][:5]
+    infected_host_test = logger.config_dict['INFECTED_HOSTS'][5:]
+    df_infected_host_train = df_selected[df_selected['ip_src'].isin(infected_host_train)]
+    df_infected_host_test = df_selected[df_selected['ip_src'].isin(infected_host_test)]
+    logger.log("Split infected host: first 5 in train, last 5 in test")
+
+    df_normal_host = df_selected[~df_selected['ip_src'].isin(infected_host_train)]
+    df_normal_host = df_normal_host[~df_normal_host['ip_src'].isin(infected_host_test)]
+    logger.log("Select normal traffic")
+
+    X_normal = df_normal_host.loc[:, df_normal_host.columns != 'label'].values
+    y_normal = df_normal_host['label'].values
+
+    X_infected_host_train = df_infected_host_train.loc[:, df_infected_host_train.columns != 'label'].values
+    y_infected_host_train = df_infected_host_train['label'].values
+
+    X_infected_host_test = df_infected_host_test.loc[:, df_infected_host_test.columns != 'label'].values
+    y_infected_host_test = df_infected_host_test['label'].values
+
+    X_train, X_test, y_train, y_test = train_test_split(X_normal, y_normal,
+                                                        test_size=0.21, random_state=13)
+
+    X_train = np.concatenate([X_train, X_infected_host_train])
+    y_train = np.concatenate([y_train, y_infected_host_train])
+
+    indices = np.arange(X_train.shape[0])
+    np.random.shuffle(indices)
+    X_train = X_train[indices]
+    y_train = y_train[indices]
+
+    X_test = np.concatenate([X_test, X_infected_host_test])
+    y_test = np.concatenate([y_test, y_infected_host_test])
+
+    logger.log("Shuffle and combine in train {} and test {}".format(
+        X_train.shape[0], X_test.shape[0]))
+    logger.log("Train label distribution {}".format(Counter(y_train)))
+
+    return X_train, y_train, X_test, y_test
 
 
 def extract_other(data_df, self_ip):
@@ -30,6 +87,14 @@ def get_estimation_error(true_freq, estimated_freq, estimation_size):
             freq_err += freq
 
     return freq_err
+
+
+def print_stats(true, predicted, logger):
+    tn, fp, fn, tp = confusion_matrix(true, predicted).ravel()
+    logger.log("TN: {}, FP: {}, FN: {}, TP: {}".format(tn, fp, fn, tp), tabs=2)
+    logger.log("Accuracy: {:.4f}".format(accuracy_score(true, predicted)), tabs=2)
+    logger.log("Precision: {:.4f}".format(precision_score(true, predicted)), tabs=2)
+    logger.log("Recall: {:.4f}".format(recall_score(true, predicted)), tabs=2)
 
 
 def plot_estimation_error(x, values, xlabel, plt_title, filename, logger):
